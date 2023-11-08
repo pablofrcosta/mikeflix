806b59f4a85f531491b9e65df653471cd6c41cf8
Pablo França <mutleydt@gmail.com>
2023-11-08 16:02:06 -0300

    atualizaçao do app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import MovieContextProvider from "./contexts/movieContext"
 import router from "./router"
+
 import { RouterProvider } from "react-router-dom"
+
 export default function App() {
   return (
     <MovieContextProvider>
